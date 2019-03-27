255bdef56eb9f99866baa4eaf182a5065795d019
adamski126 <adamski126@gmail.com>
2019-03-27 14:06:19 +0100

    test3

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("test1");
         System.out.println("test2");
+        System.out.println("test3");
 
     }
 
